d4b241b76e74e849113eb05d08aa96d54fde7836
github username <github email address>
2017-08-29 16:06:00 -0400

    done

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,46 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Define the initial state:
+    this.state = { errors: [], user: null, settings:
+      {bitrate: 8, video: { resolution: '1080p' } }
+    }
+  }
+
+  bitrate = (event) => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12,
+      },
+    })
+  }
+
+  resolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        },
+      },
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.bitrate}>{this.state.settings.bitrate}</button>
+        <button className="resolution"
+          onClick={this.resolution}>{this.state.settings.video.resolution}</button>
+      </div>
+    )
+  }
+}
+
+export default YouTubeDebugger;
